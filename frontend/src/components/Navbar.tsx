8318d8379f3f22ab2b58fb15df6a5764e5c8c0c2
Pratik <pratiky2000@gmail.com>
2025-10-16 10:29:30 +0530

    feat: add Navbar component for site navigation

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import { Button, Container, Flex } from '@mantine/core'
+import Image from 'next/image'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <Container w="fit-content" pt={20}>
+        <Flex justify="center" align="center" gap={24} className='navbar'>
+            <Image src="/cmwlogo.svg" alt="logo" width={44} height={44} />
+            <nav>
+                <ul className=''>
+                    <li>Home</li>
+                    <li>Find Jobs</li>
+                    <li>Find Talents</li>
+                    <li>About us</li>
+                    <li>Testimonials</li>
+                </ul>
+            </nav>
+            <div>
+                <Button className='create-btn'>Create Jobs</Button>
+            </div>
+        </Flex>
+    </Container>
+  )
+}
+
+export default Navbar
